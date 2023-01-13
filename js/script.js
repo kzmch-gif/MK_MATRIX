d74fdcb25f99e47dca19516d149bb779210fcfdf
kzmch-gif <milenakuzmich@gmail.com>
2023-01-13 16:47:52 +0100

    test comit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// document.getElementsByTagName("dataBirth")[0].value;
+// document.getElementsByTagName("maleId")[0].value;
+// document.getElementsByTagName("languageId")[0].value;
+
+
+
+let firstName = document.getElementsByTagName("firstName").value;
+
+console.log(firstName);
+function getElement () {
+
+}
