d35eaba03acc689488f4fc3de7bd147c1a199d5f
kzmch-gif <milenakuzmich@gmail.com>
2023-01-16 13:33:32 +0100

    ended if day>22 || day ==0

--- js/script.js
+++ js/script.js
@@ -5,24 +5,30 @@ function someFunc() {
     let dataBirth = document.getElementById("dataBirth").value;
     let maleId = document.getElementById("maleId").value;
     let languageId = document.getElementById("languageId").value;
-    
+
     function trim_date() {
         //получаем первый символ
         let a = dataBirth[0];
         let b = dataBirth[1];
-
+        let ab = a + b;
         //преобразование типов в число
         var a_number = Number(a);
         var b_number = Number(b);
-        var day; 
+        var ab_number = Number(ab);
+        var day;
+
 
         //проверка на число больше 22 и 0 перед цифрой
-        if (a_number == 0 || dataBirth > 22){
+        if (a_number == 0 || ab_number > 22) {
             day = a_number + b_number;
-            alert(day);
+
+        } else {
+            return alert("Помилка вводу дня");
         }
+
+        alert(day);
     }
-    
+
     trim_date();
 }
 
